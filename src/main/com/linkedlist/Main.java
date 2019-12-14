85b01d45b243ea822244b43a90ce9dd4f566a73d
rpestana18 <Dbmg1986>
2019-12-14 12:22:07 -0300

    Put the Main Class for Linked List on the linked List folder

---
+++ src/main/com/linkedlist/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import main.com.linkedlist.model.Player;
+import main.com.linkedlist.operation.LinkedList;
+import main.com.linkedlist.util.GenerateMembers;
+import main.com.linkedlist.util.Interface;
+
+/**
+ * Class to execute the linked list
+ *
+ * @author Rodrigo Pestana
+ */
+public class Main {
+
+    public static void main(String... args) {
+
+        LinkedList list = new LinkedList();
+        for (Player player : GenerateMembers.generateList())
+            list.insertNewPlayer(player);
+
+        Interface.showMemberOfTheLinkedList(list.listMembers());
+    }
+}
